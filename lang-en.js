3568c783a72c7b9be5e58f82a6fdd0fef4963dda
ciro-123 <58786066+ciro-123@users.noreply.github.com>
2025-10-30 18:15:19 +0000

    errores fix

--- lang-en.js
+++ lang-en.js
@@ -2,36 +2,36 @@ const langEN = {
   mainTitle: "Ciudad de las Nubes",
   mainDesc: " ",
   timeline: [
-    { id: "phase1", label: "Legends", title: "Legends", content: "Bohemundo has created the Sun for you. Alone, facing the Abyss, he drove the stars away from our land. Julia, Helion, Apopino; this new world, carved far from the abysmal darkness, is yours.", img: "imgs/etapas/leyenda.png",
+    { id: "phase1", label: "Legends", title: "Legends", content: "Bohemundo has created the Sun for you. Alone, facing the Abyss, he drove the stars away from our land. Julia, Helion, Apopino; this new world, carved far from the abysmal darkness, is yours.", img: "imgs/icono.png",
       items: [
         { label: "Eiche's Compassion", href: "#", previewText: " ", previewImg: "imgs/wip.png", state: "wip" }
       ]
     },
-    { id: "phase2", label: "Conquest", title: "Conquest", content: "After the war against the angels, the descendants of Helion have claimed their ancestral right over humanity. Enock and his children lead the conquest with the blessing of the heavens, for all must be theirs.", img: "imgs/etapas/leyenda.png",
+    { id: "phase2", label: "Conquest", title: "Conquest", content: "After the war against the angels, the descendants of Helion have claimed their ancestral right over humanity. Enock and his children lead the conquest with the blessing of the heavens, for all must be theirs.", img: "imgs/icono.png",
       items: [
         { label: "Pong In Caroteo", href: "https://2high2work.itch.io/pong-in-caroteo", previewText: " ", previewImg: "imgs/portadas/pci.png", state: "released" },
         { label: "The Carnivore Sun", href: "#", previewText: " ", previewImg: "imgs/wip.png", state: "wip" }
       ]
     },
-    { id: "phase3", label: "Dominion", title: "Dominion", content: "The Heliones wither in the Ciudad de las Nubes, with the conquered lands at their feet. Now, the scientific revolution of the Gathai, the gold of the Medicit, and the forges of the Lennox threaten the hegemony of the Titan’s direct descendants.", img: "imgs/etapas/leyenda.png",
+    { id: "phase3", label: "Dominion", title: "Dominion", content: "The Heliones wither in the Ciudad de las Nubes, with the conquered lands at their feet. Now, the scientific revolution of the Gathai, the gold of the Medicit, and the forges of the Lennox threaten the hegemony of the Titan’s direct descendants.", img: "imgs/icono.png",
       items: [
       ]
     },
-    { id: "phase4", label: "Decline", title: "Decline", content: "The Imperial High Command has surrendered. The seams of the vast Empire, once Enock’s dream, unravel as his descendants lie forgotten on distant battlefields. No Helions remain in the City of Clouds, and in the chambers of grand palaces, visions of better worlds are dreamed.", img: "imgs/etapas/leyenda.png",
+    { id: "phase4", label: "Decline", title: "Decline", content: "The Imperial High Command has surrendered. The seams of the vast Empire, once Enock’s dream, unravel as his descendants lie forgotten on distant battlefields. No Helions remain in the City of Clouds, and in the chambers of grand palaces, visions of better worlds are dreamed.", img: "imgs/icono.png",
       items: [
         { label: "Verrevelas", href: "#", previewText: " ", previewImg: "imgs/wip.png", state: "wip" }
       ]
     },
-    { id: "phase5", label: "Liberation", title: "Liberation", content: "The Ciudad de las Nubes has fallen. Julia, freed from her prison, has begun the conquest of her late brother’s ancient lands. Humanity breathes again, once more liberated from the curse of the Bent Soul imposed by the cruel heavens.", img: "imgs/etapas/leyenda.png",
+    { id: "phase5", label: "Liberation", title: "Liberation", content: "The Ciudad de las Nubes has fallen. Julia, freed from her prison, has begun the conquest of her late brother’s ancient lands. Humanity breathes again, once more liberated from the curse of the Bent Soul imposed by the cruel heavens.", img: "imgs/icono.png",
       items: [
         { label: "Night of Wolves", href: "https://store.steampowered.com/app/2393490/Night_of_Wolves", previewText: " ", previewImg: "imgs/portadas/now.jpg", state: "released" }
       ]
     },
-    { id: "phase6", label: "Total War", title: "Total War", content: "After centuries of reign, the Daughter of Bohemundo has been slain upon her throne. The sky has shattered, and the earth roars for the blood spilled. Has humanity condemned itself? Have they lost the favor of the heavens? Has the grace of the Sun abandoned them?", img: "imgs/etapas/leyenda.png",
+    { id: "phase6", label: "Total War", title: "Total War", content: "After centuries of reign, the Daughter of Bohemundo has been slain upon her throne. The sky has shattered, and the earth roars for the blood spilled. Has humanity condemned itself? Have they lost the favor of the heavens? Has the grace of the Sun abandoned them?", img: "imgs/icono.png",
       items: [
       ]
     },
-    { id: "phase7", label: "Celestial Towers", title: "Celestial Towers", content: "Under the shadow of the fractured sky, angels and humans were pardoned in the Council of Adrahan. United by the same destiny, they must hold the heavens strong; with the same strength that Bohemundo holds the Sun; with the same strength that Helion holds the Moon above our heads.", img: "imgs/etapas/leyenda.png",
+    { id: "phase7", label: "Celestial Towers", title: "Celestial Towers", content: "Under the shadow of the fractured sky, angels and humans were pardoned in the Council of Adrahan. United by the same destiny, they must hold the heavens strong; with the same strength that Bohemundo holds the Sun; with the same strength that Helion holds the Moon above our heads.", img: "imgs/icono.png",
       items: [
         { label: "The Tower", href: "#", previewText: " ", previewImg: "imgs/wip.png", state: "wip" }
       ]
--- lang-es.js
+++ lang-es.js
@@ -2,36 +2,36 @@ const langES = {
   mainTitle: "Ciudad de las Nubes",
   mainDesc: " ",
   timeline: [
-    { id: "phase1", label: "Leyendas", title: "Leyendas", content: "Bohemundo ha creado para vosotros el Sol. Sólo, frente al Abismo, alejó a las estrellas de nuestra tierra. Julia, Helión, Apopino; vuestro es este nuevo mundo esculpido lejos de la oscuridad abismal.", img: "imgs/etapas/leyenda.png",
+    { id: "phase1", label: "Leyendas", title: "Leyendas", content: "Bohemundo ha creado para vosotros el Sol. Sólo, frente al Abismo, alejó a las estrellas de nuestra tierra. Julia, Helión, Apopino; vuestro es este nuevo mundo esculpido lejos de la oscuridad abismal.", img: "imgs/icono.png",
       items: [
         { label: "Eiche's Compassion", href: "#", previewText: " ", previewImg: "imgs/wip.png", state: "wip"}
       ]
     },
-    { id: "phase2", label: "Conquista", title: "Conquista", content: "Tras la guerra contra los ángeles, los descendientes de Helión han reclamado su derecho ancestral sobre la humanidad. Enock y sus hijos lideran la conquista con la bendición de los cielos, pues todo ha de ser suyo.", img: "imgs/etapas/leyenda.png",
+    { id: "phase2", label: "Conquista", title: "Conquista", content: "Tras la guerra contra los ángeles, los descendientes de Helión han reclamado su derecho ancestral sobre la humanidad. Enock y sus hijos lideran la conquista con la bendición de los cielos, pues todo ha de ser suyo.", img: "imgs/icono.png",
       items: [
         { label: "Pong In Caroteo", href: "https://2high2work.itch.io/pong-in-caroteo", previewText: " ", previewImg: "imgs/portadas/pci.png", state: "released"},
         { label: "The Carnivore Sun", href: "#", previewText: " ", previewImg: "imgs/wip.png", state: "wip"}
       ]
     },
-    { id: "phase3", label: "Dominio", title: "Dominio", content: "Los Heliones se marchitan en la Ciudad de las Nubes, con la tierra conquistada a sus pies. Ahora, la revolución científica de los Gathai, las monedas de los Medicit, y las fraguas de los Lennox amenazan la hegemonía de los descendientes directos del Titán.", img: "imgs/etapas/leyenda.png",
+    { id: "phase3", label: "Dominio", title: "Dominio", content: "Los Heliones se marchitan en la Ciudad de las Nubes, con la tierra conquistada a sus pies. Ahora, la revolución científica de los Gathai, las monedas de los Medicit, y las fraguas de los Lennox amenazan la hegemonía de los descendientes directos del Titán.", img: "imgs/icono.png",
       items: [
       ]
     },
-    { id: "phase4", label: "Decadencia", title: "Decadencia", content: "El Alto Mando Imperial ha renunciado. Las costuras del vasto Imperio, que fue el sueño de Enock, se deshace mientras sus descendientes yacen olvidados en los lejanos campos de batalla. Ya no quedan Heliones en la Ciudad de las Nubes, y en las cámaras de los grandiosos palacios se sueñan mundos mejores.", img: "imgs/etapas/leyenda.png",
+    { id: "phase4", label: "Decadencia", title: "Decadencia", content: "El Alto Mando Imperial ha renunciado. Las costuras del vasto Imperio, que fue el sueño de Enock, se deshace mientras sus descendientes yacen olvidados en los lejanos campos de batalla. Ya no quedan Heliones en la Ciudad de las Nubes, y en las cámaras de los grandiosos palacios se sueñan mundos mejores.", img: "imgs/icono.png",
       items: [
         { label: "Verrevelas", href: "#", previewText: " ", previewImg: "imgs/wip.png", state: "wip"}
       ]
     },
-    { id: "phase5", label: "Liberación", title: "Liberación", content: "La Ciudad de las Nubes ha caído. Julia, liberada de su prisión, ha iniciado la conquista de las antiguas tierras de su difunto hermano. La humanidad respira de nuevo, liberada una vez más de la maldición del Alma Doblegada impuesta por los crueles cielos.", img: "imgs/etapas/leyenda.png",
+    { id: "phase5", label: "Liberación", title: "Liberación", content: "La Ciudad de las Nubes ha caído. Julia, liberada de su prisión, ha iniciado la conquista de las antiguas tierras de su difunto hermano. La humanidad respira de nuevo, liberada una vez más de la maldición del Alma Doblegada impuesta por los crueles cielos.", img: "imgs/icono.png",
       items: [
         { label: "Night of Wolves", href: "https://store.steampowered.com/app/2393490/Night_of_Wolves", previewText: " ", previewImg: "imgs/portadas/now.jpg", state: "released"},
       ]
     },
-    { id: "phase6", label: "Guerra Total", title: "Guerra Total", content: "Tras siglos de reinado, han asesinado a la Hija de Bohemundo en su trono. El cielo se ha partido, y la tierra ruge por la sangre derramada.  ¿Acaso la humanidad se ha condenado? ¿Perdieron el favor de los cielos? ¿Les abandonó la gracia del Sol?", img: "imgs/etapas/leyenda.png",
+    { id: "phase6", label: "Guerra Total", title: "Guerra Total", content: "Tras siglos de reinado, han asesinado a la Hija de Bohemundo en su trono. El cielo se ha partido, y la tierra ruge por la sangre derramada.  ¿Acaso la humanidad se ha condenado? ¿Perdieron el favor de los cielos? ¿Les abandonó la gracia del Sol?", img: "imgs/icono.png",
       items: [
       ]
     },
-    { id: "phase7", label: "Torres Celestiales", title: "Torres Celestiales", content: "Bajo la sombra del cielo fracturado, ángeles y humanos fueron perdonados en el Concilio de Adrahan. Unidos por el mismo destino, deben sostener el cielo con fuerza; con la misma fuerza que Bohemundo sostiene el Sol; con la misma fuerza que Helión sostiene sobre nuestras cabezas la Luna.", img: "imgs/etapas/leyenda.png",
+    { id: "phase7", label: "Torres Celestiales", title: "Torres Celestiales", content: "Bajo la sombra del cielo fracturado, ángeles y humanos fueron perdonados en el Concilio de Adrahan. Unidos por el mismo destino, deben sostener el cielo con fuerza; con la misma fuerza que Bohemundo sostiene el Sol; con la misma fuerza que Helión sostiene sobre nuestras cabezas la Luna.", img: "imgs/icono.png",
       items: [
         { label: "The Tower", href: "#", previewText: " ", previewImg: "imgs/wip.png", state: "wip"}
       ]
