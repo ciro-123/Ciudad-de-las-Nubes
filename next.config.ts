02dd53f45f62d310cc182919f1d695a400f5e357
ciro-123 <58786066+ciro-123@users.noreply.github.com>
2026-08-05 16:48:11 +0000

    Actualizar flujo de trabajo de despliegue y configuración de Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Outputs static HTML/CSS/JS to an 'out' folder
+  // images: { unoptimized: true }, // Uncomment if you use next/image components
 };
 
 export default nextConfig;
